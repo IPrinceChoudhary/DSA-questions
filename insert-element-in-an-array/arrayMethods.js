e622dc9a6583adadb2c9359a1bde02129459bf46
Prince Choudhary <princechoudhary784@gmail.com>
2024-09-04 11:47:01 +0530

    insert-element-in-an-array-with-different-approaches

--- insert-element-in-an-array/arrayMethods.js
+++ insert-element-in-an-array/arrayMethods.js
@@ -16,17 +16,38 @@ let newValue = 69;
 // console.log(data)
 
 
-// SLICE and CONCAT method 
+// SLICE and CONCAT and SPREAD OPERATOR method 
 
 // slice method slices down the elements like this [start,end], returns a portion in a form of array in which the sliced items are there, slice doesn't modify the original array, also a way to make a copy of an array
 
 // concat method concatenates(joins) two or more arrays .concat(array), returns a new array containing the joined arrays, doesn't modify the original array, also a way to make a copy of an array as the old and concatenated array will both be a different array
 
-const slicedArrayFromLast = array.slice(position, array.length);
-const slicedArrayFromStart = array.slice(0, position)
-const concatNewItem = slicedArrayFromStart.concat(newValue, slicedArrayFromLast)
-// console.log(concatNewItem)
+// spread operator is a syntax feature which helps in copying merging or expanding arrays, it does not return anything directly instead it is used to expand or spread elements, it does not modify the original array, also a way to make a copy of an array
 
-// short way 
-const newArray = array.slice(0,position).concat(newValue).concat(array.slice(position))
+// const slicedArrayFromLast = array.slice(position, array.length);
+// const slicedArrayFromStart = array.slice(0, position)
+// const concatNewItem = slicedArrayFromStart.concat(newValue, slicedArrayFromLast)
+// // console.log(concatNewItem)
+
+// // short way 
+// const newArray = array.slice(0,position).concat(newValue).concat(array.slice(position))
+// console.log(newArray)
+
+// spread operator and slice
+
+// const newArray = [...array.slice(0,position), newValue, ...array.slice(position)]
+// console.log(newArray)
+
+
+// reduceRight method 
+
+const newArray = array.reduceRight((arr, curr, index)=>{
+  if(index >= position){
+    arr[index + 1] = curr
+  }
+  if(index === position){
+    arr[index] = newValue
+  }
+  return arr
+},[...array])
 console.log(newArray)
