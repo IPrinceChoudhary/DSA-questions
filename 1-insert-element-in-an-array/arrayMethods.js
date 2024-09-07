4afb5e01545b06613e81b366927a8b0aef72f066
Prince Choudhary <princechoudhary784@gmail.com>
2024-09-07 18:13:09 +0530

    dsa questions solution with different ways

--- 1-insert-element-in-an-array/arrayMethods.js
+++ 1-insert-element-in-an-array/arrayMethods.js
@@ -8,8 +8,6 @@ let newValue = 69;
 
 // 1... SPLICE Method
 
-//splice method start from the position default is 0 then deletes default 0 and the items to add inside an array, splice returns removed items in a new array if 0 items is to delete it will return an empty array, splice does modify the original array
-
 array.splice(position, 0, newValue);
 // const data = array.splice(position, 1, newValue); // remove an item and add removed item in an array
 // console.log(data)
@@ -19,12 +17,6 @@ console.log(array)
 
 
 // 2... SLICE and CONCAT and SPREAD OPERATOR method
-
-// slice method slices down the elements like this [start,end], returns a portion in a form of array in which the sliced items are there, slice doesn't modify the original array, also a way to make a copy of an array
-
-// concat method concatenates(joins) two or more arrays .concat(array), returns a new array containing the joined arrays, doesn't modify the original array, also a way to make a copy of an array as the old and concatenated array will both be a different array
-
-// spread operator is a syntax feature which helps in copying merging or expanding arrays, it does not return anything directly instead it is used to expand or spread elements, it does not modify the original array, also a way to make a copy of an array
 
 // const slicedArrayFromLast = array.slice(position, array.length);
 // const slicedArrayFromStart = array.slice(0, position)
@@ -43,8 +35,6 @@ console.log(array)
 
 // 3... reduceRight method
 
-// reduceRight() works similarly to reduce(), but processes the array elements from right to left. accepts a callback function and initial value as 2 arguments. callback function can have 4 arguments accumulator(accumulated value from the previous call), current value, current index, array(where it is called upon)
-
 // const newArray = array.reduceRight((arr, curr, index)=>{
 //   if(index >= position){
 //     arr[index + 1] = curr
@@ -59,8 +49,6 @@ console.log(array)
 
 // 4... Array.from method
 
-// Array.from method creates a new array instance and allows us to convert or manipulate items from original array into a new array. args - arrayLike(Set, Node list, normal array), mapping function (transforms each element of the array-like object into a new value)
-
 // const newArray = Array.from({ length: array.length + 1 }, (_, i) => { // _ placeholder for the first parameter of the mapping function, which is the current value being processed. In this case, it’s not used (hence the underscore).
 //   return i < position ? array[i] : i === position ? newValue : array[i - 1]
 // })
@@ -73,9 +61,15 @@ console.log(array)
 
 // 5... Map method with Flat
 
-// array.map method creates a new array by applying a specified function to each element of the original array. does not modify the original array. args- element, index, array(optional). 
-
 // const newArray = array.map((element, index)=>
 //   index === position ? [newValue, element] : element
 // ).flat()
 // console.log(newArray);
+
+
+// 6... flatMap
+
+// const newArray = array.flatMap((element, index)=>{
+//   return index === position ? [newValue, element] : element
+// })
+// console.log(newArray)
--- 2-delete-element-in-an-array/arrayMethods.js
+++ 2-delete-element-in-an-array/arrayMethods.js
@@ -6,40 +6,91 @@ const array = [23, 53, 62, 16, 73, 9];
 
 const position = 3;
 
-// array.splice(position, 1)
-// console.log(array)
+array.splice(position, 1)
+console.log(array)
 
 
-// SLICE and CONCAT
+// Slice and Concat
 
 // const newArray = array.slice(0, position).concat(array.slice(position + 1))
 // console.log(newArray)
 
 
-// SPREAD OPERATOR and SLICE
+// Spread Operator and Slice
 
 // const newArray = [...array.slice(0, position), ...array.slice(position+1)]
 // console.log(newArray)
 
 
-// FILTER
-
-// The filter() method in JavaScript is an array method that creates a new array with all elements that pass a test (or satisfy a condition) provided by a callback function. It does not modify the original array, but returns a new one. Parameters: callback(element, index(optional), array(optional)), this(optional). also ignores empty slots in the array.
+// Filter
 
 // const newArray = array.filter((items) => !(items === array[position])); // it will also filter out, if the value is repeating in array
 // console.log(newArray);
-// Or 
+// Or
 // const newArray = array.filter((_, index)=> {
 //   return !(index === position)
 // })
 // console.log(newArray)
 
-// NOTE: the method is more precise if we want to remove the element according to the position in that array(removes only the element at the specified index, even if the array has duplicate values).
-// Use the first approach if you want to remove all occurrences of a specific value in the array.
-// Use the second approach if you want to remove only the element at a specific position, regardless of its value.
+// NOTE: 
 
 
-// REDUCE
+// Reduce
 
-// This method iterates over an array, applying a reducer function to each element, accumulating a single output value. It takes an initial value and processes elements from left to right, reducing the array to a single result. 2 parameters:- cb function(total, currentValue, currentIndex(optional), array(optional)), initialValue(optional). returns a single value/element after traversing the complete array.
+// const newArray = array.reduce((acc, curr, index) => {
+  // return index !== position ? [...acc, curr] : acc
+//   // OR 
+//   if(index !== position){
+//     acc.push(curr)
+//   }
+//   return acc
+// }, []);
 
+// console.log(newArray);
+
+
+// ReduceRight
+
+// const newArray = array.reduceRight((acc, curr, index)=> index !== position ? [curr, ...acc] : acc, [])
+// const newArray = array.reduceRight((acc, curr, index)=>{
+//   if(index !== position){
+//     acc.unshift(curr)
+//   }
+//   return acc
+// }, [])
+
+// console.log(newArray)
+
+
+// Array.from
+
+// here
+// const newArray = Array.from(array, (value, index)=> index !== position ? value : null).filter(val => val)
+// const newArray = Array.from(array, (value, index)=> index !== position && value).filter(Boolean)
+// const newArray = Array.from({length: array.length}, (_, index) => index !== position && array[index]).filter(Boolean)
+// console.log(newArray)
+
+
+// Map 
+
+// const newArray = array.map((value, index)=> index !== position && value ).filter(Boolean)
+// console.log(newArray)
+
+
+// flatMap 
+
+// const newArray = array.flatMap((element, index)=> index === position ? [] : element ) // flatMap() is designed to flatten the result. it gets flattened and essentially removed from the final result.
+// const newArray = array.flatMap((element, index)=> index !== position && element).filter(Boolean)
+// console.log(newArray)
+
+
+// forEach
+
+// const newArray = []
+// array.forEach((element, index)=>{
+//   if(index !== position){
+//     newArray.push(element)
+//   }
+// })
+// console.log(newArray)
+// console.log(array)
