f5c2a9904370736d2235cd667bf52b49b42e1c36
Prince Choudhary <princechoudhary784@gmail.com>
2025-03-23 08:51:42 +0530

    added Condition of doing the output without using push method(polyfill of push method), rest parameters and arguments array like object

--- 1-insert-element-in-an-array/forLoop(moving-forward-approach).js
+++ 1-insert-element-in-an-array/forLoop(moving-forward-approach).js
@@ -5,17 +5,17 @@ const array = [23, 53, 62, 16, 73, 9];
 const position = 3;
 let newValue = 69;
 
-// increasing the length of array first
-array.length = array.length + 1;
+// // increasing the length of array first
+// array.length = array.length + 1;
 
-// forward moving array
-for(i = position; i < array.length; i++){
-  let temp = array[i];
-  array[i] = newValue;
-  newValue = temp
-}
+// // forward moving array
+// for(i = position; i < array.length; i++){
+//   let temp = array[i];
+//   array[i] = newValue;
+//   newValue = temp
+// }
 
-console.log(array);
+// console.log(array);
 
 // NOTE :- The backward-moving array approach is generally faster because it directly shifts elements without requiring temporary storage or extra operations. It directly moves each element to the next index, ensuring that no values are overwritten or stored temporarily.
 
@@ -30,16 +30,41 @@ console.log(array);
 // array.push(newValue)
 // console.log(array)
 
+
 // Push adds elements in the last of an array, modifies the original array, return the length of modified array 
+
+// CONDITION:- do same but using without push(make polyfill)
+
+// for (let i = position; i < array.length; i++) {
+//   let temp = array[i]
+//   array[i] = newValue;
+//   newValue = temp
+// }
+
+// Array.prototype.pushElement = function(...args){ // rest parameters. will wrap values in an actual array, ES6.
+//   let length = this.length;
+//   for(let i = 0; i<args.length; i++){
+//     this[length] = args[i]; 
+//     length++;
+//   }
+//   return length
+// }
+// // NOTE:- we can do the same with using arguments. arguments is an array-like object containing all passed values. arguments is used in older versions and not supported in arrow function, also can't use array methods
+
+// array.pushElement(newValue)
+// console.log(array)
+
 
 
 // CONDITION 2 :- do with making an empty array with new Array()
-// let newArray = new Array(array.length + 1) // creates a new array with empty items// in this case (6+1)
-// for(i = 0; i < position; i++){ // The loop control (for (let i = 0; i < position; i++)) only manages the index i. It doesn't need to reference the array directly because it's only responsible for iterating over the indices.
-//   newArray[i] = array[i] // The reference to array happens inside the loop body: newArray[i] = array[i];. This is where the elements of array are accessed and assigned to newArray.
-// }
-// newArray[position] = newValue;
-// for(i = position; i<array.length; i++){
-//   newArray[i + 1] = array[i]
-// }
-// console.log(newArray)
+let newArray = new Array(array.length + 1) // creates a new array with empty items// in this case (6+1)
+
+for(i = 0; i < position; i++){ // The loop control (for (let i = 0; i < position; i++)) only manages the index i. It doesn't need to reference the array directly because it's only responsible for iterating over the indices.
+  newArray[i] = array[i] // The reference to array happens inside the loop body: newArray[i] = array[i];. This is where the elements of array are accessed and assigned to newArray.
+}
+console.log(newArray)
+newArray[position] = newValue;
+for(i = position; i<array.length; i++){
+  newArray[i + 1] = array[i]
+}
+console.log(newArray)
