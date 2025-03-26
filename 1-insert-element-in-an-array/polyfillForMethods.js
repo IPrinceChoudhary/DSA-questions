a3a539dc5c37ac4bc04feea3968d03b8850efd8c
Prince Choudhary <princechoudhary784@gmail.com>
2025-03-26 10:38:22 +0530

    polyfill for methods

---
+++ 1-insert-element-in-an-array/polyfillForMethods.js
@@ -0,0 +1,75 @@
+// polyfill for push method - in the forLoop(forward-approach.js file)
+
+
+
+
+// polyfill for slice method
+
+// let example = [23,45,62,33,23435,52,35];
+// let position = 4;
+// let start = 0
+
+// Array.prototype.sliceArray = function(start, position){
+//   let newArray = new Array(position - start);
+//   let index = 0;
+//   for(let i = start; i<position; i++){
+//     newArray[index] = this[i];
+//     index++
+//   }
+//   return newArray;
+// }
+
+// const result = example.sliceArray(start, position);
+// console.log(result);
+
+
+
+
+// polyfill for concat method 
+
+// const firstArray = [1,2,3]
+// const secondArray = [4,5,6]
+// let oldArray = []
+
+// Array.prototype.concatArray = function(...arrays){
+//   let length = this.length;
+//   for(let i = 0; i<arrays.length; i++){
+//     length += arrays[i].length
+//   }
+//   let newArray = new Array(length);
+//   let index = 0;
+//   for(let i = 0; i<this.length; i++){
+//     newArray[index] = this[i]
+//     index++
+//   }
+//   for(let i = 0; i<arrays.length; i++){
+//     let arr = arrays[i]
+//     for(let j = 0; j<arr.length; j++){
+//       newArray[index] = arr[j];
+//       index++
+//     }
+//   }
+//   return newArray
+// }
+
+// const result = oldArray.concatArray(firstArray, secondArray)
+// console.log(result)
+
+
+
+
+// polyfill for spread operator
+function spreadPolyfill(arr) {
+  let newArray = new Array(arr.length);
+  
+  for (let i = 0; i < arr.length; i++) {
+    newArray[i] = arr[i];
+  }
+  
+  return newArray;
+}
+
+// Usage
+const original = [1, 2, 3];
+const copy = spreadPolyfill(original);
+console.log(copy);
