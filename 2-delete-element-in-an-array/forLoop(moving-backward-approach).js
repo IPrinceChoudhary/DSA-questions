4a800935b99210dbc3bf7ca8810b423bf13a82e4
Prince Choudhary <princechoudhary784@gmail.com>
2025-03-30 07:58:51 +0530

    re written the logic which only works for that length of an array

--- 2-delete-element-in-an-array/forLoop(moving-backward-approach).js
+++ 2-delete-element-in-an-array/forLoop(moving-backward-approach).js
@@ -4,13 +4,11 @@ const array = [23, 53, 62, 16, 73, 9];
 
 const position = 3;
 
-for(let i = array.length-1; i >= position; i--){
-  if(i === position){
-    for(let i = position; i < array.length; i++){
-      array[i] = array[i + 1]
-    }
-  }
+for (let i = array.length - 1; i > position + 1; i--) { // if we change the array values, then it won't work
+  let temp = array[i - 1];
+  array[i - 1] = array[i];
+  array[i - 2] = temp
 }
-array.length--
+array.length--;
 
-console.log(array)
+console.log(array);
