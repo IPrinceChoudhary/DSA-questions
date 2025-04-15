d8a804cf5a3b24e83412943718ad75bdaed22b86
Prince Choudhary <princechoudhary784@gmail.com>
2025-04-15 08:39:25 +0530

    custom sorting and checking repeated items logic

--- 5-merge-two-arrays-with-sort/forLoop.js
+++ 5-merge-two-arrays-with-sort/forLoop.js
@@ -9,36 +9,34 @@ let resultArray = [];
 // CONDITION : must be unique items(if item repeats in array don't include in the resultArray)
 
 let found;
-for(let i = 0; i<firstArray.length; i++){
-  found = false; // no same item is presented in the array
 
-  for(let k = 0; k<resultArray.length; k++){ // checking if the one item is already there in the resultArray
-    if(firstArray[i] === resultArray[k]){
-      found = true 
-      break; // if yes then don't execute the further code for that repeated item
+for(let i = 0; i<firstArray.length; i++){
+  found = false
+  for(let j = 0; j<resultArray.length; j++){
+    if(firstArray[i] === resultArray[j]){
+      found = true
+      break
     }
   }
 
-  if(!found){ // if the item is not repeated
-    let inserted = false // default; value isn't inserted yet in the resultArray
-    // main execution 
-    for(let j=0; j < resultArray.length; j++){
-      if(firstArray[i] < resultArray[j]){ // check whether the item is smaller than the items already presented in the result array
-        for(let k = resultArray.length; k > j; k--){ // start from the length not last item, loop will go till the index of the result array item (item where we want to shift the item to the next index)
-          resultArray[k] = resultArray[k-1] // it will duplicate the item of the last index and it will go further k > j
-// NOTE: k++ approach would access an out-of-bounds index, and the array may not behave as expected
-        } // now in the result array one item is repeating
-        resultArray[j] = firstArray[i] // overwrite the repeated item 
+  if(!found){
+    let inserted = false;
+    for(let j = 0; j<resultArray.length; j++){
+      if(firstArray[i] < resultArray[j]){
+        for(let k = resultArray.length; k > j; k--){
+          resultArray[k] = resultArray[k-1];
+        }
+        resultArray[j] = firstArray[i];
         inserted = true;
         break
       }
     }
-    // the one case could be that the item is bigger than every item of resultArray
     if(!inserted){
-      resultArray[resultArray.length] = firstArray[i] // adding to length not last index coz it will overwrite the last index
+      resultArray[resultArray.length] = firstArray[i]
     }
   }
 }
+
 // same logic for the secondArray
 for(let i = 0; i<secondArray.length; i++){
   found = false;
